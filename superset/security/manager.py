8e6ea0b5fc47de17c2d78232e56a83ee8248ba42
Ahmed Habeeb <ahmedhabeeb556@gmail.com>
2025-07-26 17:10:09 +0300

    Update manager.py

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -447,7 +447,7 @@ class SupersetSecurityManager(  # pylint: disable=too-many-public-methods
         if catalog:
             return f"[{database}].[{catalog}].[{schema}]"
 
-        return f"[{catalog}].[{schema}]"
+        return f"[{database}].[{schema}]"
 
     @staticmethod
     def get_database_perm(database_id: int, database_name: str) -> Optional[str]:
